f07d6314eb5623127013a9bb45cee19e7f77f70a
Vitorepf <83190724+Vitorepf@users.noreply.github.com>
2023-05-20 02:49:13 -0300

    add

---
+++ script.js
@@ -0,0 +1,238 @@
+var cantadas = {
+    pt: [
+      { texto: "Meu amor por você é como o universo, infinito e em constante expansão." },
+      { texto: "Seu sorriso ilumina minha vida como o sol ilumina o mundo." },
+      { texto: "Quando estou com você, o tempo parece parar. Cada segundo ao seu lado é precioso." },
+      { texto: "Você deve ser um contrato bem redigido, porque você é perfeita em todos os termos." },
+  { texto: "Meu amor por você é como uma causa jurídica: irrefutável e incontestável." },
+  { texto: "Você é a testemunha que prova que o amor é o melhor argumento." },
+  { texto: "Eu me declaro culpado de amar você além de todas as objeções." },
+  { texto: "Você é minha defesa pessoal contra a monotonia e a solidão." },
+  { texto: "O amor que sinto por você não precisa de recurso, é uma decisão final." },
+  { texto: "Cada contrato de vida precisa de uma cláusula especial chamada 'nós dois juntos'." },
+  { texto: "Você é o caso mais importante da minha vida, e eu defendo esse amor com todas as minhas forças." },
+  { texto: "Você é a jurista do meu coração, e eu sou condenado a amar você para sempre." },
+  { texto: "Nossa relação é como um habeas corpus, libertou meu coração e trouxe felicidade para minha vida." },
+  { texto: "Você é a excelência em pessoa, minha jurista favorita." },
+  { texto: "O seu amor é uma prova incontestável da felicidade que eu encontrei." },
+  { texto: "Nosso amor é uma aliança inquebrável, mais forte do que qualquer cláusula contratual." },
+  { texto: "Você é minha causa ganha, minha razão para sorrir todos os dias." },
+  { texto: "Meu coração é seu tribunal e nele você sempre será absolvida de qualquer acusação." },
+  { texto: "Você tem o poder de me convencer de qualquer coisa, inclusive de te amar para sempre." },
+  { texto: "Nossa relação é a melhor defesa contra a solidão e a tristeza." },
+  { texto: "Você é minha testemunha ocular de um amor que transcende todas as barreiras." },
+  { texto: "Nosso amor é imune a qualquer recurso, é uma sentença final de felicidade." },
+  { texto: "Meu coração é seu escritório particular, onde você pode encontrar todo o meu amor e dedicação." },
+  { texto: "Você é a advogada do meu coração, e eu sou réu confesso de te amar loucamente." },
+  { texto: "Você é a prova viva de que o amor é a melhor causa para se lutar." },
+  { texto: "Nossa relação é um contrato vitalício, com cláusulas de amor eterno e felicidade constante." },
+  { texto: "Você tem o poder de me incriminar com um simples olhar, mas eu sou culpado de te amar além das palavras." },
+  { texto: "Você é a única testemunha que eu preciso para provar que o nosso amor é incondicional." },
+  { texto: "Seu sorriso ilumina meu mundo e faz todos os problemas desaparecerem." },
+  { texto: "Desde que te conheci, o amor se tornou a trilha sonora da minha vida." },
+  { texto: "Estar ao seu lado é como viver em um sonho que nunca quero acordar." },
+  { texto: "Você é a personificação do amor em todas as suas formas." },
+  { texto: "Seu abraço é o meu refúgio, onde encontro paz e conforto." },
+  { texto: "Cada momento ao seu lado é um presente valioso que guardo no coração." },
+  { texto: "Seu amor é a essência que preenche minha alma e me faz sentir completo(a)." },
+  { texto: "Estar contigo é como caminhar nas nuvens, é uma sensação única e incrível." },
+  { texto: "Você é a resposta para todas as minhas preces e a realização dos meus sonhos." },
+  { texto: "Nosso amor é como um conto de fadas, onde cada página é repleta de magia e encanto." }
+    ],
+    en: [
+      { texto: "Are you a magician? Because whenever I look at you, everyone else disappears." },
+      { texto: "Do you have a map? I keep getting lost in your eyes." },
+      { texto: "If you were a vegetable, you'd be a cute-cumber." },
+      { texto: "You are the reason why my sun rises every morning." },
+  { texto: "Every moment with you feels like a dream come true." },
+  { texto: "You are the missing piece that completes my puzzle." },
+  { texto: "Being with you is like a fairytale, a storybook romance." },
+  { texto: "Your love is like a melody that echoes in my heart." },
+  { texto: "In your eyes, I found the reflection of a love so pure and true." },
+  { texto: "You are the light that guides me through the darkest days." },
+  { texto: "My heart beats to the rhythm of your love." },
+  { texto: "You are my safe haven, my shelter from the storm." },
+  { texto: "Your voice is as soothing as a gentle caress." },
+  { texto: "Your love fills my soul with joy and happiness." },
+  { texto: "With you, every day is an exciting adventure." },
+  { texto: "Your smile melts my heart like butter." },
+  { texto: "You are the most beautiful star in my sky." },
+  { texto: "I fell in love with you at first sight, and every day since then, my love has grown stronger." },
+  { texto: "You are my dream come true, the realization of all my hopes and desires." },
+  { texto: "Your love is the most beautiful story of my life." },
+  { texto: "I love you more than anything in the world." },
+  { texto: "You are my sunshine, my moonlight, my stars." },
+  { texto: "In your arms, I have found home, a place where I truly belong." }
+    ],
+    es: [
+      { texto: "¿Crees en el amor a primera vista o debo pasar otra vez?" },
+      { texto: "Eres como el sol, iluminas mi mundo." },
+      { texto: "Eres el sueño que nunca quiero despertar." },
+      { texto: "¿Crees en el amor a primera vista o debo pasar otra vez?" },
+      { texto: "Eres como el sol, iluminas mi mundo." },
+    { texto: "Eres el sueño que nunca quiero despertar." },
+    { texto: "Si el amor fuera un delito, estaría cumpliendo cadena perpetua contigo." },
+    { texto: "Eres el dulce suspiro que escapa de mis labios." },
+    { texto: "Cada vez que te miro, mi corazón sonríe." },
+    { texto: "Eres mi razón para creer en el amor verdadero." },
+    { texto: "Tu amor es la melodía que alegra mi alma." },
+    { texto: "Eres el destino al que siempre quise llegar." },
+    { texto: "En tus ojos encuentro el reflejo de mi felicidad." },
+    { texto: "Eres el abrazo que necesito en cada amanecer." },
+    { texto: "Eres mi refugio en medio de la tormenta." },
+    { texto: "Eres el beso que nunca quiero dejar de probar." },
+    { texto: "Eres la pieza que completa el rompecabezas de mi vida." },
+    { texto: "Eres el sueño que nunca quiero despertar." },
+    { texto: "Eres el latido que acelera mi corazón." },
+    { texto: "Eres el tesoro más valioso que he encontrado." },
+    { texto: "Eres la respuesta a todas mis preguntas." },
+    { texto: "Eres el motivo por el que sonrío cada día." },
+    { texto: "Eres mi historia de amor favorita." },
+    { texto: "Eres la evidencia de que el amor siempre gana el caso." },
+  { texto: "Tu amor es una sentencia inapelable de felicidad." },
+  { texto: "Eres mi excepción a todas las objeciones del corazón." },
+  { texto: "Mi corazón te declara culpable de robarte mis pensamientos." },
+  { texto: "Eres mi defensa personal contra la tristeza y la soledad." },
+  { texto: "Nuestro amor no necesita apelación, es una decisión final." },
+  { texto: "Cada contrato de vida necesita una cláusula especial llamada 'nosotros juntos'." },
+  { texto: "Eres la jueza de mi corazón y yo soy el acusado de amarte sin medidas." },
+  { texto: "Tú eres la jurista de mis sueños, siempre alegando a favor de nuestro amor." },
+  { texto: "Nuestro amor es inadmisible a cualquier recurso, es una sentencia de felicidad eterna." },
+  { texto: "Eres la excepción a todas las reglas, el veredicto de mi corazón." },
+  { texto: "Eres la prueba irrefutable de que el amor es la mejor argumentación." },
+  { texto: "Nuestra relación es un contrato vitalicio con cláusulas de amor incondicional." },
+  { texto: "Tu amor es el recurso perfecto para cualquier pena y dolor." },
+  { texto: "Eres mi testigo de cargo, el testimonio de un amor verdadero." }
+    ],
+    fc: [
+        { texto: "Je ne peux pas imaginer ma vie sans toi." },
+  { texto: "Ton sourire illumine ma journée." },
+  { texto: "Tu es mon bonheur, ma joie de vivre." },
+  { texto: "Chaque instant avec toi est un cadeau précieux." },
+  { texto: "Ton amour est la plus belle mélodie à mes oreilles." },
+  { texto: "Tu es ma raison de croire en l'amour véritable." },
+  { texto: "Dans tes yeux, je trouve la magie et la tendresse." },
+  { texto: "Tu es la lumière qui guide mes pas." },
+  { texto: "Mon cœur bat au rythme du tien." },
+  { texto: "Tu es mon refuge, mon havre de paix." },
+  { texto: "Ta voix est douce comme une caresse." },
+  { texto: "Ton amour remplit mon âme de bonheur." },
+  { texto: "Avec toi, chaque jour est une aventure passionnante." },
+  { texto: "Ton sourire fait fondre mon cœur." },
+  { texto: "Tu es la plus belle étoile dans mon ciel." },
+  { texto: "Je suis tombé(e) amoureux/amoureuse de toi au premier regard." },
+  { texto: "Tu es mon rêve devenu réalité." },
+  { texto: "Ton amour est la plus belle histoire de ma vie." },
+  { texto: "Je t'aime plus que tout au monde." },
+  { texto: "Tu es mon soleil, ma lune, mes étoiles." },
+  { texto: "Tu es la preuve irréfutable que mon cœur t'appartient." },
+  { texto: "Si l'amour était un délit, tu serais ma complice parfaite." },
+  { texto: "Tu es le verdict de tous mes rêves, toujours en ta faveur." },
+  { texto: "Notre amour est une cause gagnée d'avance, sans aucun doute." },
+  { texto: "Avec toi, chaque jour est un plaidoyer pour le bonheur." },
+  { texto: "Ton amour est la loi suprême dans mon cœur." },
+  { texto: "Je plaide coupable de t'aimer sans réserve." },
+  { texto: "Toutes les objections de mon cœur ont été rejetées grâce à toi." },
+  { texto: "Je demande à être condamné à une peine d'amour à perpétuité avec toi." },
+  { texto: "Notre relation est un contrat indissoluble, scellé par l'amour." },
+  { texto: "Tu es mon avocat de l'amour, toujours à mes côtés pour me défendre." },
+  { texto: "Je suis prêt(e) à plaider devant le tribunal de ton cœur pour obtenir ton amour." },
+  { texto: "Mon cœur est prêt à témoigner de mon amour pour toi devant le monde entier." },
+  { texto: "Tu es mon argument principal pour la joie et le bonheur dans ma vie." },
+  { texto: "Notre amour est la loi fondamentale de notre bonheur." }
+      ]
+  };
+  
+  var player;
+  
+  function gerarCantada() {
+    var language = document.getElementById("language").value;
+    var cantadasIdioma = cantadas[language];
+    var index = Math.floor(Math.random() * cantadasIdioma.length);
+    var cantada = cantadasIdioma[index];
+  
+    var cantadaTexto = document.getElementById("cantadaTexto");
+  
+    cantadaTexto.textContent = cantada.texto;
+  
+    var cantadaMediaContainer = document.getElementById("cantadaMediaContainer");
+    cantadaMediaContainer.innerHTML = '';
+  
+    var media = getRandomMedia();
+  
+    if (media.type === 'image') {
+      var img = document.createElement("img");
+      img.src = media.src;
+      img.alt = "Cantada Image";
+      img.classList.add("cantadaMedia");
+      cantadaMediaContainer.appendChild(img);
+    } else if (media.type === 'video') {
+      var video = document.createElement("video");
+      video.src = media.src;
+      video.autoplay = true;
+      video.controls = true;
+      video.playsinline = true;
+      video.classList.add("cantadaMedia");
+      cantadaMediaContainer.appendChild(video);
+      player = new Plyr(video);
+    }
+  
+    var card = document.querySelector('.card');
+    card.classList.remove('show');
+    setTimeout(function() {
+      card.classList.add('show');
+    }, 300);
+  }
+  
+  function changeLanguage() {
+    gerarCantada();
+  }
+  
+  function getRandomMedia() {
+    var mediaFiles = [
+      { src: "assets/imagem1.jpeg", type: 'image' },
+      { src: "assets/video1.mp4", type: 'video' },
+      { src: "assets/imagem2.jpeg", type: 'image' },
+      { src: "assets/video2.mp4", type: 'video' },
+      { src: "assets/imagem3.jpeg", type: 'image' },
+      { src: "assets/video3.mp4", type: 'video' },
+      { src: "assets/video4.mp4", type: 'video' },
+      { src: "assets/imagem4.jpeg", type: 'image' },
+      { src: "assets/imagem5.jpeg", type: 'image' },
+      { src: "assets/imagem6.jpeg", type: 'image' },
+      { src: "assets/imagem7.jpeg", type: 'image' },
+      { src: "assets/imagem8.jpeg", type: 'image' },
+      { src: "assets/imagem9.jpeg", type: 'image' },
+      { src: "assets/imagem10.jpeg", type: 'image' },
+      { src: "assets/imagem11.jpeg", type: 'image' },
+      { src: "assets/imagem12.jpeg", type: 'image' },
+      { src: "assets/video5.mp4", type: 'video' },
+      { src: "assets/video6.mp4", type: 'video' },
+      { src: "assets/video7.mp4", type: 'video' },
+      { src: "assets/video8.mp4", type: 'video' },
+      { src: "assets/video9.mp4", type: 'video' },
+      { src: "assets/video10.mp4", type: 'video' },
+      { src: "assets/imagem13.jpeg", type: 'image' },
+      { src: "assets/imagem14.jpeg", type: 'image' },
+      { src: "assets/imagem15.jpeg", type: 'image' },
+      { src: "assets/imagem16.jpeg", type: 'image' },
+      { src: "assets/imagem17.jpeg", type: 'image' },
+      { src: "assets/imagem18.jpeg", type: 'image' },
+      { src: "assets/imagem19.jpeg", type: 'image' },
+      { src: "assets/imagem20.jpeg", type: 'image' },
+      { src: "assets/imagem21.jpeg", type: 'image' },
+    ]; // Adicione mais nomes de arquivos de mídia conforme necessário
+  
+    var index = Math.floor(Math.random() * mediaFiles.length);
+    return mediaFiles[index];
+  }
+  
+  document.getElementById("gerarBtn").addEventListener("click", gerarCantada);
+
+
+var img = document.createElement("img");
+img.src = media.src;
+img.alt = "Cantada Image";
+img.classList.add("cantadaMedia");
+cantadaMediaContainer.appendChild(img);
+  
